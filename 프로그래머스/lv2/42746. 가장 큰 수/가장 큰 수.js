a919de99aa400cbab2eba285b90b0969f6e432ca
minji <73338341+metamoong@users.noreply.github.com>
2023-01-22 11:00:00 +0900

    [level 2] Title: 가장 큰 수, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.js
@@ -1,13 +1,27 @@
 function solution(numbers) {
     let answer = '';
     numbers.sort((a,b)=>{
-        return Number(b.toString()+a.toString()) - Number(a.toString() + b.toString());
+        const str_a = a.toString();
+        const str_b = b.toString();
+        let idx_a=0, idx_b =0 ;
+        while(idx_a<str_a.length&&idx_b<str_b.length){
+            if(Number(str_a[idx_a])>Number(str_b[idx_b])) return -1;
+            if(Number(str_a[idx_a])<Number(str_b[idx_b])) return 1;
+            idx_a +=1;
+            idx_b +=1;
+        }
+        if(idx_a===str_a.length&&idx_b === str_b.length) return 0;
+        if(idx_a === str_a.length){
+            if(Number(str_b[idx_b])>Number(str_b[0])) return 1;
+             return -1;
+        }
+         if(idx_b === str_b.length){
+            if(Number(str_a[idx_a])>Number(str_a[0])) return -1;
+             return 1;
+        }
     });
     for(let num of numbers){
         answer += num;
     }
-    
-    if(numbers[0] === 0) return '0';
-    
     return answer;
 }

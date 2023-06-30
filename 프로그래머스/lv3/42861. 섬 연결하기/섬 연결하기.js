6ebd43cb9684d10fcac9930fe6aa8bc3a855ca44
minji <73338341+metamoong@users.noreply.github.com>
2023-07-01 07:46:23 +0900

    [level 3] Title: 섬 연결하기, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.js
+++ 프로그래머스/lv3/42861. 섬 연결하기/섬 연결하기.js
@@ -15,24 +15,20 @@ function solution(n, costs) {
     isVisited[costs[0][0]] = true;
     isVisited[costs[0][1]] = true;
 
+    
+    let sum =costs[0][2];
     while(use.length<n-1){
         for(let i = 0 ;i<costs.length;i+=1){
             if(isValid(costs[i])){
                 use.push(costs[i]);
+                sum+=costs[i][2];
                 isVisited[costs[i][0]] = true;
                 isVisited[costs[i][1]] = true;
                 break;
             };
         }
-        console.log(use)
-    
     }
     
-    let sum =0;
-    for(let i = 0;i<use.length;i+=1){
-        sum += use[i][2];
-    }
-
+ 
     return sum;
 }
-

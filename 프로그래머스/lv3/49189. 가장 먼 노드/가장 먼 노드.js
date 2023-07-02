6689ef7c22a5df21dd58c8b673506af6bcde9b64
minji <73338341+metamoong@users.noreply.github.com>
2023-07-02 10:49:42 +0900

    [level 3] Title: 가장 먼 노드, Time: 41.17 ms, Memory: 56.1 MB -BaekjoonHub

--- 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
+++ 프로그래머스/lv3/49189. 가장 먼 노드/가장 먼 노드.js
@@ -9,30 +9,23 @@ function solution(n, edge) {
     });
 
     const isVisited = new Array(n).fill(false);
-    const fars = [];
-    let far = 0;
     const q = [];
-
-    isVisited[0] = true;
-    graph[0].forEach((v)=>{
-        isVisited[v-1] = true;
-        q.push(v);
-    });
-    
+    let far = 0;
     let cnt = 0;
+
+    q.push(1);
+    isVisited[0] = true;
+
     while(q.length>0){
         const len = q.length;
         far +=1;
         cnt = len;
         for(let i = 0;i<len;i+=1){
             const ver = q.shift();
-            fars[ver-1] = far;
-            const list = graph[ver-1];
-            list.forEach((v)=>{
-                if(!isVisited[v-1]){
-                    isVisited[v-1] = true;
-                    q.push(v);
-                };
+            [...graph[ver-1]].forEach((v)=>{
+                if(isVisited[v-1]) return;
+                isVisited[v-1] = true;
+                q.push(v);
             });
         }
     }

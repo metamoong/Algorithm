504dbbdce2ddad7985339a3ca4e9de95bbb29f8d
minji <73338341+metamoong@users.noreply.github.com>
2023-08-06 11:23:23 +0900

    [level 3] Title: N으로 표현, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv3/42895. N으로 표현/N으로 표현.js
+++ 프로그래머스/lv3/42895. N으로 표현/N으로 표현.js
@@ -1,8 +1,6 @@
 function solution(N, number) {
     var answer = 0;
     const memo = Array.from({length:9},()=>[]);
-    memo[1].push(N);
-    if(N===number ) return 1;
     
     const addAvailables = (i,j)=>{
         memo[i].forEach((num1)=>{
@@ -12,21 +10,19 @@ function solution(N, number) {
                 memo[i+j].push(num2/num1);
                 memo[i+j].push(num1/num2);
                 memo[i+j].push(num1-num2);
-          
                
             })
         });
     }
     
-    
-    for(let i = 2;i<9;i+=1){
+    for(let i = 1;i<9;i+=1){
+         memo[i].push(Number((N.toString()).repeat(i)));
         for(let j = 1;j<=Math.floor(i/2);j+=1){
-            memo[i].push(Number((N.toString()).repeat(i)));
             addAvailables(j,i-j);
-            if(memo[i].includes(number)){
-                return i;
-            }
         }
+        if(memo[i].includes(number)){
+            return i;
+         }
         
     }
     return -1;

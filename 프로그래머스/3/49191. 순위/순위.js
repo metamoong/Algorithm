8d3cffd6d0432781a84a6cd14ddbea4baafacce5
minji <73338341+metamoong@users.noreply.github.com>
2023-11-10 12:01:46 +0900

    [level 3] Title: 순위, Time: 9.56 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/3/49191. 순위/순위.js
@@ -0,0 +1,36 @@
+function solution(n, results) {
+    let answer = 0;
+    const map = Array.from({length:n},()=>new Array(n).fill(-1));
+    
+    results.forEach(result=>{
+        [winner,loser] = result;
+        const wIdx = winner-1;
+        const lIdx = loser-1;
+        map[wIdx][lIdx] = 1;
+        map[lIdx][wIdx] = 0;
+    });
+    
+      for(let i = 0;i<n;i+=1){
+        for(let j = 0;j<n;j+=1){
+            if(map[i][j]===1){
+                for(let k = 0;k<n;k+=1){
+                    if(map[i][k]===0){
+                        map[k][j] = 1;
+                        map[j][k] = 0;
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    map.forEach(arr=>{
+        let cnt = 0;
+        arr.forEach(result=>{
+            if(result!==-1) cnt+=1;
+        })
+        if(cnt===n-1) answer+=1;
+    })
+    
+    return answer;
+}
